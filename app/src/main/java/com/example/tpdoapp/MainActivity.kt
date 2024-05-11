c9e83255c640dad08a616047a56b03db2d9cc38d
UdulaThathsaridu <it22056320@my.sliit.lk>
2024-05-11 09:40:33 +0530

    added Crud implementations

--- app/src/main/java/com/example/tpdoapp/MainActivity.kt
+++ app/src/main/java/com/example/tpdoapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.tpdoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.tpdoapp.database.TaskDatabase
+import com.example.tpdoapp.repository.TaskRepository
+import com.example.tpdoapp.viewmodel.TaskViewModel
+import com.example.tpdoapp.viewmodel.TaskViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var taskViewModel: TaskViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupViewModel()
+    }
+
+    private fun setupViewModel() {
+        val taskRepository = TaskRepository(TaskDatabase(this))
+        val viewModelProviderFactory=TaskViewModelFactory(application,taskRepository)
+        taskViewModel = ViewModelProvider(this,viewModelProviderFactory)[TaskViewModel::class.java]
+
     }
 }
